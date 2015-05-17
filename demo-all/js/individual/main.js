5c837464e30df1abdddcbd9786d720b808993429
ralf <kingarthas37@hotmail.com>
2015-05-17 20:49:40 +0800

    git

--- demo-all/js/individual/main.js
+++ demo-all/js/individual/main.js
@@ -1,21 +1,67 @@
-global.jQuery = require('jquery'); 
+global.jQuery = require('jquery');
 var swfobject = require('swfobject');
 var flashDetect = require('flash-detect');
 
- require('jcarousel');
-
-$(function() {
-    
-    
-    
-    
-    console.log(swfobject);
-    
-    console.log(flashDetect.installed);
+require('jcarousel');
+require('jquery-cookie');
+require('jquery-validate');
 
 
-    $('.jcarousel').jcarousel();
-    
-    
-    
-});
+(function($,window) {
+    $(function() {
+ 
+
+        console.log(swfobject);
+
+        console.log(flashDetect.installed);
+
+
+         
+
+
+
+        $.cookie('user2323','kingarthas');
+
+        $("#commentForm").validate();
+
+
+
+
+
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+
+    });
+
+})(jQuery,window);
+ 
